c8413e5d64e32136265bbebe52e498ec618d7eac
Daniel Moreira <danieldenis01@gmail.com>
2021-04-24 13:19:53 -0300

    adiciona teste para Post

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  it { expect(described_class.ancestors).to include ApplicationRecord }
+
+  it do
+    post = Post.new
+    post.valid?
+
+    expect(post.errors[:title].present?).to be_truthy
+  end
+
+  it do
+    post = Post.new title: 'O melhor post do mundo'
+    post.valid?
+
+    expect(post.errors[:title].present?).to be_falsey
+  end
+end
